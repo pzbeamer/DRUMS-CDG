21274d7edafc2834648fb9302adad8f6cc36d38f
Caroline Hammond <cellh@udel.edu>
2021-06-24 10:24:15 -0400

    Almost completed pots extractor

--- AStract/POTS_checka.m
+++ AStract/POTS_checka.m
@@ -4,21 +4,24 @@ load('../Summary_Data_800_Gals/summary.mat','betweenTimes');
 pots_possibles=cell(872,1);
 count=1;
 
-for pt=3:158
+for pt=3:872
+   
+    T{pt,1}{1}
     if isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'))
-
+        disp("isfile");
         load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
         
         %rest_ind=1;
         %end_ind=last index
         start_ind=find(abs(Tdata-AS_start)==min(abs(Tdata-AS_start)));
+        end_avg_ind=find(abs(Tdata-(AS_start-5))==min(abs(Tdata-(AS_start-5))));
         if betweenTimes(2,pt-2)<30
             begin_avg_ind=1;
         else
-            begin_avg_ind=find(abs(Tdata-AS_start-30)==min(abs(Tdata-AS_start-30)));
+            begin_avg_ind=find(abs(Tdata-(AS_start-30))==min(abs(Tdata-(AS_start-30))));
         end
 
-        avg_HR_before=mean(Hdata(begin_avg_ind:start_ind));
+        avg_HR_before=mean(Hdata(begin_avg_ind:end_avg_ind));
         maxHR=max(movmean(Hdata(start_ind:end),50));
 
         if (maxHR>=avg_HR_before+30 && T{pt,3}>19) || (maxHR>=avg_HR_before+40)
