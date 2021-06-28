062d6019b21dda8cb0a2be05b85718becdd4a007
gallegos9 <gallegon@bu.edu>
2021-06-28 12:22:20 -0400

    fixed POTS_checka_HUT and created POTS_checka_both combining AS and HUT checkas

--- AStract/POTS_checka_HUT.m
+++ AStract/POTS_checka_HUT.m
@@ -15,15 +15,15 @@ for pt=3:872
         %rest_ind=1;
         %end_ind=last index
         start_ind=find(abs(Tdata-HUT_start)==min(abs(Tdata-HUT_start)));
-        end_avg_ind=find(abs(Tdata-(HUT_start-10))==min(abs(Tdata-(HUT_start-10))));
-        if betweenTimes(1,pt-2)<40
+        end_avg_ind=find(abs(Tdata-(HUT_start-15))==min(abs(Tdata-(HUT_start-15))));
+        if betweenTimes(1,pt-2)<60
             begin_avg_ind=1;
         else
-            begin_avg_ind=find(abs(Tdata-(HUT_start-40))==min(abs(Tdata-(HUT_start-40))));
+            begin_avg_ind=find(abs(Tdata-(HUT_start-60))==min(abs(Tdata-(HUT_start-60))));
         end
 
         avg_HR_before=median(Hdata(begin_avg_ind:end_avg_ind));
-        maxHR=max(movmean(Hdata(start_ind:end), 50));
+        maxHR=max(movmean(Hdata(start_ind:end), 100));
 
         if (maxHR>=avg_HR_before+30 && T{pt,3}>19) || (maxHR>=avg_HR_before+40)
             disp(strcat(T{pt,1}," Meets Qualifications"));
---
+++ AStract/POTS_checka_both.m
@@ -0,0 +1,62 @@
+clear all
+T=readtable('../Summary_Data_800_Gals/PatientInfo062221.csv');
+load('../Summary_Data_800_Gals/summary.mat','betweenTimes');
+pots_pats=cell(872,1);
+oldcount=1;
+newcount=1;
+c=0;
+
+for pt=3:872
+   
+    T{pt,1}{1}
+    if isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'))
+        disp("isfile");
+        load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
+        c=c+1;
+        %rest_ind=1;
+        %end_ind=last index
+        start_ind_a=find(abs(Tdata-AS_start)==min(abs(Tdata-AS_start)));
+        end_avg_ind_a=find(abs(Tdata-(AS_start-5))==min(abs(Tdata-(AS_start-5))));
+        if betweenTimes(2,pt-2)<30
+            begin_avg_ind_a=1;
+        else
+            begin_avg_ind_a=find(abs(Tdata-(AS_start-30))==min(abs(Tdata-(AS_start-30))));
+        end
+
+        avg_HR_before_a=median(Hdata(begin_avg_ind_a:end_avg_ind_a));
+        maxHR_a=max(movmean(Hdata(start_ind_a:end), 50));
+
+        if (maxHR_a>=avg_HR_before_a+30 && T{pt,3}>19) || (maxHR_a>=avg_HR_before_a+40)
+            disp(strcat(T{pt,1}," Meets Qualifications"));
+            pots_pats(oldcount)=T{pt,1};
+            newcount=newcount+1;
+        end
+    
+    elseif (isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/HUT/',T{pt,1}{1},'_HUT_WS.mat')) && oldcount==newcount)
+        disp("isfile");
+        load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/HUT/',T{pt,1}{1},'_HUT_WS.mat'));
+        c=c+1;
+        %rest_ind=1;
+        %end_ind=last index
+        start_ind_h=find(abs(Tdata-HUT_start)==min(abs(Tdata-HUT_start)));
+        end_avg_ind_h=find(abs(Tdata-(HUT_start-15))==min(abs(Tdata-(HUT_start-15))));
+        if betweenTimes(1,pt-2)<60
+            begin_avg_ind_h=1;
+        else
+            begin_avg_ind_h=find(abs(Tdata-(HUT_start-60))==min(abs(Tdata-(HUT_start-60))));
+        end
+
+        avg_HR_before_h=median(Hdata(begin_avg_ind_h:end_avg_ind_h));
+        maxHR_h=max(movmean(Hdata(start_ind_h:end), 100));
+
+        if (maxHR_h>=avg_HR_before_h+30 && T{pt,3}>19) || (maxHR_h>=avg_HR_before_h+40)
+            disp(strcat(T{pt,1}," Meets Qualifications"));
+            pots_pats(oldcount)=T{pt,1};
+            newcount=newcount+1;
+        end
+    end
+    
+    if oldcount~=newcount
+        oldcount=newcount;
+    end
+end
