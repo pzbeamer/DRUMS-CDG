25b68a87061cc92a5d77fb20aeed18b87cf7ff87
gallegos9 <gallegon@bu.edu>
2021-06-28 16:41:43 -0400

    There are 2 versions of POTS_checka_both, the first checking if the current patient is the same as the previous, and the second using uniqueTotals.

--- AStract/POTS_checka_both_1.m
+++ AStract/POTS_checka_both_1.m
@@ -4,12 +4,32 @@ load('../Summary_Data_800_Gals/summary.mat','betweenTimes');
 pots_pats=cell(872,1);
 oldcount=1;
 newcount=1;
+oldpat='HPV1';
 c=0;
 
 for pt=3:872
-   
+  % check if this patient is the same as the last patient
+    newpat = 'HPV';
+    ind = 4; %index
+    ptname = T{pt,1}{1}; %patient number
+    if ~strcmp(ptname(1),'H') %if the patient doesn't start with 'HPV', add it on
+        ptname = strcat('HPV',ptname);
+    end 
+    addletters = ptname(ind); %letter to concatenate
+       
+    while ~isnan(str2double(addletters)) %go through each index of patname until you reach a non-number
+        newpat = strcat(newpat, addletters);
+        ind = ind+1;
+        if ind <= strlength(ptname)
+            addletters = ptname(ind);
+        else
+            addletters = '.'; %if the index is greater than the length of patname, break out of loop
+        end    
+    end
+
+    
     T{pt,1}{1}
-    if isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'))
+    if (isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat')) && ~strcmp(newpat,oldpat))
         disp("isfile");
         load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
         c=c+1;
@@ -31,8 +51,9 @@ for pt=3:872
             pots_pats(oldcount)=T{pt,1};
             newcount=newcount+1;
         end
-    
-    elseif (isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/HUT/',T{pt,1}{1},'_HUT_WS.mat')) && oldcount~=newcount)
+    end
+
+    if (isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/HUT/',T{pt,1}{1},'_HUT_WS.mat')) && oldcount==newcount && ~strcmp(newpat,oldpat))
         disp("isfile");
         load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/HUT/',T{pt,1}{1},'_HUT_WS.mat'));
         c=c+1;
@@ -55,6 +76,10 @@ for pt=3:872
             newcount=newcount+1;
         end
     end
-    if oldcount~=newcount
-        oldcount=new
+
+    if (oldcount~=newcount && ~strcmp(newpat,oldpat))
+        oldcount=newcount;
+    end
+    
+    oldpat=newpat;
 end
