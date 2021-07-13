3264d9f5f39a277b84c6d80d1dfce3d2125a9d2a
tess <tjones26@ncsu.edu>
2021-07-13 16:49:43 -0400

    idk

--- Summary_Data_800_Gals/copyFiler.m
+++ Summary_Data_800_Gals/copyFiler.m
@@ -1,12 +1,12 @@
 clear all
 T = readtable('PatientInfo07082021.csv','Headerlines',2);
-for pt = 1:250
+for pt = 250:500
     pt_id = T{pt,1}{1}
   
     pt_file_name = strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/Vals_New/'...
                ,pt_id,'_val1_WS.mat');
     if isfile(pt_file_name)
-        directory = '../../Valsalvas4Opt/';
+        directory = '../../Vals_New/';
         copyfile(pt_file_name, directory)
     end
 end
