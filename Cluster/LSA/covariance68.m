5aad3918e2ac86f6056c46d80703ad4efc5ef410
tess <tjones26@ncsu.edu>
2021-07-13 14:14:02 -0400

    update

--- Cluster/LSA/covariance68.m
+++ Cluster/LSA/covariance68.m
@@ -12,8 +12,6 @@ load('Sens/sensHPV68_20150112_val1.mat')
 %INDMAP = [1 6 8 14 15 20]; 
 %(1,14),(1,15),(14,15) correlated > 0.95, remove 1
 %INDMAP = [6 8 14 15 20]; 
-%none correlated, remove 1
-INDMAP = [6 8 14 15 20]; 
 %none correlated
 
 S = sens(:,INDMAP);
