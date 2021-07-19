0f5bcc80e946f800f64e686ae9fafb22c7caddd1
pzbeamer <perry.beamer@gmail.com>
2021-07-19 09:26:19 -0400

    optimized

--- Optimized_Stats/flag_checka.m
+++ Optimized_Stats/flag_checka.m
@@ -1,47 +1,47 @@
 %% No saveDat
-T=readtable('../DRUMS-CDG/Cluster/Residual_Algorithm/PatientInfo07132021.csv');
+T=readtable('../Cluster/Residual_Algorithm/PatientInfo07132021.csv');
 
-ct=1;
+ct=0;
 %'HPV592_20150825_optimized.mat','HPV592_20151211_optimized.mat','HPV593_20160419_optimized.mat'
 %pt = 654,655,656
 %have no saveDat, suggesting major workspace issue/ spreadsheet
-for pt=500:699
+for pt=3:250
     pt_id=T{pt,1}{1};
-    if isfile(strcat(pt_id,'_optimized.mat'))
-        load(strcat(pt_id,'_optimized.mat'))
+    if isfile(strcat('../Optimized/',pt_id,'_optimized.mat'))
+        load(strcat('../Optimized/',pt_id,'_optimized.mat'))
         try
             saveDat;
             clear saveDat;
         catch
             pt
-            flags{1,ct}=pt_id;
+            flags{1,ct+1}=pt_id;
             ct=ct+1;
         end
     end
 end
     
 %% Maximum Time Cutoff and Divergence
-T=readtable('../DRUMS-CDG/Cluster/Residual_Algorithm/PatientInfo07132021.csv');
+T=readtable('../Cluster/Residual_Algorithm/PatientInfo07132021.csv');
 
-ct1=1;
-ct2=1;
+ct1=0;
+ct2=0;
 ct3=0;
 ctfile=0;
 
 %35 reach maximum time cutoff
 %38 have divergence in optimal fits
 
-for pt=[500:653 657:699]
+for pt=[3:250]
     pt_id=T{pt,1}{1}
-    if isfile(strcat(pt_id,'_optimized.mat'))
+    if isfile(strcat('../Optimized/',pt_id,'_optimized.mat'))
         ctfile=ctfile+1;
-        load(strcat(pt_id,'_optimized.mat'))
+        load(strcat('../Optimized/',pt_id,'_optimized.mat'))
         if saveDat.flag(1)==1
-            flagBadErr{1,ct1}=pt_id;
+            flagBadErr{1,ct1+1}=pt_id;
             ct1=ct1+1;
         end
         if saveDat.flag(2)==1
-            flagDiverge{1,ct2}=pt_id;
+            flagDiverge{1,ct2+1}=pt_id;
             ct2=ct2+1;
         end
         if saveDat.flag(1)==1 && saveDat.flag(2)==1
