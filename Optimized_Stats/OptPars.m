c6394615e647fab0e2d2c1efddbe7a7df5393251
pzbeamer <perry.beamer@gmail.com>
2021-07-19 16:47:10 -0400

    new opts

--- Optimized_Stats/OptPars.m
+++ Optimized_Stats/OptPars.m
@@ -1,7 +1,8 @@
 
 clear all
-T=readtable('../Summary_Data_800_Gals/PatientInfo07132021.csv');
+T=readtable('../Summary_Data_800_Gals/PatientInfo07192021.csv');
 load('../AStract/potspats.mat')
+load('../Cluster/Markers/markers.mat')
 guy = 1;
 for pt = 3:872 
     
@@ -13,7 +14,9 @@ for pt = 3:872
         if ~any(saveDat.flag)
             
             opt_pars(guy,1:5) = saveDat.optpars(1,1:5);
+            opt_pars(guy,6:7) = barkers(pt-3,:);
             guy = guy +1;
+            
             if ~isempty(pots_pats(pt-2))
             
                 ppl(guy) = 1;
