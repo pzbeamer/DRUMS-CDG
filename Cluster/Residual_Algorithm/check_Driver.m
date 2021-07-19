2107b54c066823ee0ebffba0ca82af14ef0d0976
Caroline Hammond <cellh@udel.edu>
2021-07-19 13:54:23 -0400

    Checked some bad

--- Cluster/Residual_Algorithm/check_Driver.m
+++ Cluster/Residual_Algorithm/check_Driver.m
@@ -1,14 +1,26 @@
-%index 30: fits are not too different, only one wayward optimization,
-%median fine
-
+%remove 1
+%for 2 best error is best fit %in bad for med
+%for 3 best fit is any but second, best error disproven % in bad for med
+%remove 4, calibration during 
+%5 fit wack but data seems fine ahhhh
+%6 data is rough doesnt look like a valsalva, fits consistent though
+%7 min error seems like best fit % in bad for med
+%8 min error works
+%9 min error works/median
+%10 median works, realized im checking all things whoops
+%11 median works
+%14 is awful throw out was in bad for med
+%15 awful was in bad for med throw out
+%17 great data terrible fit????
+%above refer to errors with falgDiverge
 function check_Driver
 close all
-    index=23;
+    index=78;
     format shortg;
 
-    load('../../../Optimized/flagDiverge.mat')
-    pt_id=flagDiverge{index};
-    load(strcat('../../../Optimized/',pt_id,'_optimized.mat'))
+    load('../../Optimized_Stats/flagBadErr.mat');
+    pt_id=flagBadErr{index};
+    load(strcat('../../Optimized/',pt_id,'_optimized.mat'));
     
     %Parameters to estimate (taupb, taus, spb, spr, Hpr)
     INDMAP = saveDat.INDMAP;
@@ -18,7 +30,7 @@ close all
     data = load_data(pt_WS);
     data = TimeCut(data,[saveDat.restTime,30]);
     %Run 7 additional optimizations with random nominal parameter values
-    for k = 1:2%1:8
+    for k = 1:8
         DriverBasicME(data,INDMAP,saveDat.optpars,k);
     end
     pt_WS
--- Cluster/Residual_Algorithm/wrapper.m
+++ Cluster/Residual_Algorithm/wrapper.m
@@ -1,9 +1,6 @@
 
-<<<<<<< HEAD
-index = 1:30;
-=======
-index = [60];
->>>>>>> 04e645314f1684ae62b4d9659b2aae0412c7933f
+
+index = [1:2 30:33];
 tic
 POTS_Driver2(index);
 toc
