41f20d711d75fa21b411609cc3d542a1115af04d
tess <tjones26@ncsu.edu>
2021-07-20 11:44:16 -0400

    pots check

--- AStract/POTS_checka_both_Real.m
+++ AStract/POTS_checka_both_Real.m
@@ -1,5 +1,5 @@
 clear all
-T=readtable('../Summary_Data_800_Gals/PatientInfo07132021.csv');
+T=readtable('../Summary_Data_800_Gals/PatientInfo07192021.csv');
 load('../Summary_Data_800_Gals/summary.mat','uniqueTimes');
 pots_pats=cell(872,1);
 oldcount=1;
@@ -7,13 +7,13 @@ newcount=1;
 c=0;
 p = 0;
 
-for pt=3:872
+for pt=3:30
     p = 0;
     T{pt,1}{1}
     if any(uniqueTimes(2,pt-2))
         c=c+1;
         p = 1;
-        if isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
+         if 0 == 1 %isfile(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
             disp("isfile");
             
             load(strcat('/Volumes/GoogleDrive/Shared drives/REU shared/LSA/AS/',T{pt,1}{1},'_AS_WS.mat'));
@@ -22,13 +22,10 @@ for pt=3:872
             %end_ind=last index
             start_ind_a=find(abs(Tdata-AS_start)==min(abs(Tdata-AS_start)));
             end_avg_ind_a=find(abs(Tdata-(AS_start-5))==min(abs(Tdata-(AS_start-5))));
-            if uniqueTimes(2,pt-2)<30
-                begin_avg_ind_a=1;
-            else
-                begin_avg_ind_a=find(abs(Tdata-(AS_start-30))==min(abs(Tdata-(AS_start-30))));
-            end
-
-            avg_HR_before_a=median(Hdata(begin_avg_ind_a:end_avg_ind_a));
+            
+            begin_avg_ind_a=find(abs(Tdata-(AS_start-AS_rest))==min(abs(Tdata-(AS_start-AS_rest))));
+          
+            avg_HR_before_a=median(Hdata(begin_avg_ind_a:end_avg_ind_a))
             maxHR_a=max(movmean(Hdata(start_ind_a:end), 50));
 
             if (maxHR_a>=avg_HR_before_a+30 && T{pt,3}>19) || (maxHR_a>=avg_HR_before_a+40)
@@ -51,17 +48,22 @@ for pt=3:872
             %end_ind=last index
             start_ind_h=find(abs(Tdata-HUT_start)==min(abs(Tdata-HUT_start)));
             end_avg_ind_h=find(abs(Tdata-(HUT_start-15))==min(abs(Tdata-(HUT_start-15))));
-            if uniqueTimes(1,pt-2)<60
-                begin_avg_ind_h=1;
-            else
-                begin_avg_ind_h=find(abs(Tdata-(HUT_start-60))==min(abs(Tdata-(HUT_start-60))));
-            end
+            
+            begin_avg_ind_h=find(abs(Tdata-(HUT_start-HUT_rest))==min(abs(Tdata-(HUT_start-HUT_rest))));
+            
 
             avg_HR_before_h=median(Hdata(begin_avg_ind_h:end_avg_ind_h));
             maxHR_h=max(movmean(Hdata(start_ind_h:end), 100));
+            
+                figure(pt-2)
+                hold on
+                plot(Tdata,Hdata,'linewidth',3)
+                yline(avg_HR_before_h+30,'r','linewidth',3)
+                xline(HUT_start,'b')
 
             if (maxHR_h>=avg_HR_before_h+30 && T{pt,3}>19) || (maxHR_h>=avg_HR_before_h+40)
                 disp(strcat(T{pt,1}," Meets Qualifications"));
+              
                 pots_pats(pt-2)=T{pt,1};
                 newcount=newcount+1;
             end
