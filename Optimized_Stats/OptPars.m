7594b2198c4ffca203450118b5554af129058c3d
pzbeamer <perry.beamer@gmail.com>
2021-07-21 09:12:47 -0400

    afjjfjjfj

--- Optimized_Stats/OptPars.m
+++ Optimized_Stats/OptPars.m
@@ -44,15 +44,39 @@ for pt = 3:872
 end
 
 %% Cluster
-% stuff = kmeans(opt_pars,2);
+ stuff = kmeans(opt_pars,2);
 % %stuff = dbscan(opt_pars,9,20);
 % figure(1);
 % silhouette(opt_pars,stuff);
 %figure(2);
 %plot(opt_pars(:,1),opt_pars(:,2),'o')
-for i = 1:5
-    
-    figure(i+5)
-    boxplot(opt_pars(:,i),'Whisker',20)
+%% Boxplots
 
+% for i = 1:5
+%     
+%     figure(i+5)
+%     boxplot(opt_pars(:,i),'Whisker',20)
+% 
+% end
+
+
+%% SVD plotting
+
+figure(10)
+hold on
+[U, S, V] = svd(opt_pars);
+Se = S(1:2,1:2);
+Ue = U(:,1:2);
+pp = Ue * Se;
+
+for i = 1: length(stuff)
+    if stuff(i) == 2
+        bb(i,1:2) = pp(i,1:2);
+        
+    else
+        rr(i,1:2) = pp(i,1:2);
+    end
 end
+
+plot(bb(:,1),bb(:,2),'o', 'Color','b')
+plot(rr(:,1),rr(:,2),'o','Color','r')
